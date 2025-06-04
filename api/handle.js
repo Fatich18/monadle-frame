4f06e20197248928bd6fee4d08ac92202be15363
Fatich18 <155333369+Fatich18@users.noreply.github.com>
2025-06-04 23:23:18 +0300

    Update handle.js

--- api/handle.js
+++ api/handle.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
   res.status(200).send(`
     <html>
       <head>
-        <meta property="fc:frame:image" content="https://i.imgur.com/r4ZqH4B.png" />
+        <meta property="fc:frame:image" content="https://framerusercontent.com/images/vyo6AWhVlK0C2Hv88Wpxb70mS4.png" />
       </head>
       <body></body>
     </html>
