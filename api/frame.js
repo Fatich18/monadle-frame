506d6866dde78c5b90d9fbefb1dafc39793d4670
Fatich18 <155333369+Fatich18@users.noreply.github.com>
2025-06-04 23:09:36 +0300

    Update frame.js

--- api/frame.js
+++ api/frame.js
@@ -1,13 +1,18 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/html");
-  res.send(`
+  res.status(200).send(`
+    <!DOCTYPE html>
     <html>
       <head>
+        <meta property="og:title" content="Monadle Word Game" />
+        <meta property="og:image" content="https://i.imgur.com/Ln5cA5J.png" />
         <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="https://placekitten.com/800/400" />
+        <meta property="fc:frame:image" content="https://i.imgur.com/Ln5cA5J.png" />
         <meta property="fc:frame:button:1" content="Start" />
-        <meta property="fc:frame:post_url" content="/api/handle" />
+        <meta property="fc:frame:post_url" content="https://monadle-frame-b75l.vercel.app/api/handle" />
       </head>
       <body></body>
-    </html>`);
+    </html>
+  `);
 }
+
